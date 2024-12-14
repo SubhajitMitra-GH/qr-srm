0c8148c4f7e9e67d1bffeb990a198ecc9389d19c
Subhajit Mitra <msubhojit182004@gmail.com>
2024-12-14 23:07:01 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import QRcode from './section/QRCode'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <QRcode/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/section/QRCode.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './QRCode.css';
+
+const QRCode = () => {
+  return (
+    <div className='QR'>
+        <div className='heading'>
+        <h1>AIU</h1>
+        </div>
+        <div className='QRcontainer'>
+            {/* <h2>QR CODE</h2> */}
+        </div>
+    </div>
+  )
+}
+
+export default QRCode
